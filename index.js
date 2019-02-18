ae2d055beee1754a071006503ae0e28de28453b3
Rocketblaster <47755918+Rocketblaster247@users.noreply.github.com>
2019-02-18 17:13:53 -0500

    Working!

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Tasky = function () {
   this.fps = 60;
   this.tasks = [];
+  this.task = 0;
 	this.overClock = false;
   this.setFPS = function (fps) {
     if (fps == this.fps) {
-      console.log("FPS was already set to " + fps);
+      console.log("FPS was already set to " + fps + "!");
     } else {
       this.fps = fps;
     }
@@ -12,11 +13,17 @@ const Tasky = function () {
   this.addTask = function (func) {
     this.tasks.push(func);
   };
-  this.execFunc = function () {
-		
-  };
-  this.execTasks = function () {
-    var func = this.execFunc();
-    
+  this.start = function () {
+    if (tasks.length <= 0) {
+      console.log("No Tasks!");
+      return;
+    }
+    (function () {
+      this.tasks[this.task](this);
+      if ((this.task ++) > = this.tasks.length) {
+        this.task = 0;
+      }
+      window.requestAnimationFrame();
+    })();
   };
 };
